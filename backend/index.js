3bed2c088a415bf5faeb5ccbe8c7d479d2472086
alex19992020 <alejandroantunez1999@gmail.com>
2024-02-05 22:52:26 -0800

    creating user apis from chatengine.io

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { default: axios } = require("axios");
 
 const app = express();
 app.use(express.json());
@@ -7,6 +8,16 @@ app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
+
+  try {
+    const r = await axios.put(
+      "https://api.chatengine.io/users/",
+      { username: username, secret: username, first_name: username },
+      { headers: { "private-key": "2000d90b-7b0e-4da1-a4a9-ee18249d3358" } }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {}
+
   return res.json({ username: username, secret: "sha256..." });
 });
 
