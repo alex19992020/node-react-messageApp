0c950346f3c812fdb587fa25516d114ae8094b62
alex19992020 <alejandroantunez1999@gmail.com>
2024-02-06 00:06:33 -0800

    added npm react chat engine kit

--- vite-frontend/src/ChatsPage.jsx
+++ vite-frontend/src/ChatsPage.jsx
@@ -1,5 +1,21 @@
-const ChatsPage = () => {
-  return <>chats...</>;
+import {
+  MultiChatSocket,
+  MultiChatWindow,
+  useMultiChatLogic,
+} from "react-chat-engine-advanced";
+
+const ChatsPage = (props) => {
+  const chatProps = useMultiChatLogic(
+    "0d0bf40c-9e52-4890-a2a4-67e691e4b96b",
+    props.user.username,
+    props.user.secret
+  );
+  return (
+    <div style={{ height: "100vh" }}>
+      <MultiChatSocket {...chatProps} />
+      <MultiChatWindow {...chatProps} style={{ height: "100%" }} />
+    </div>
+  );
 };
 
 export default ChatsPage;
